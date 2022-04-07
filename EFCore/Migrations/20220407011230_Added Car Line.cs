b6d12db324cc0dbc7df665ce118ea9a98f69ae83
Devora Hoberman <devorahoberman1gmail.com>
2022-04-06 21:13:44 -0400

    Add project files.

---
+++ EFCore/Migrations/20220407011230_Added Car Line.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Migrations
+{
+    public partial class AddedCarLine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Car",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Car",
+                table: "People");
+        }
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+}
